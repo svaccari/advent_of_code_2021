314f1af13cbef040f105103f294084c96d7e1a93
s.vaccari <s.vaccari@ebw.it>
2021-12-12 21:11:30 +0100

    day 12 done

--- 12.kt
+++ 12.kt
@@ -1,5 +1,5 @@
 val nodes = mutableListOf<Node>()
-val paths = mutableListOf<String>()
+var paths = mutableListOf<String>()
 
 fun main() {
     for (i in 0 until input.size) {
@@ -22,21 +22,31 @@ fun main() {
             }
         }
     }
-    nodes.forEach { println(it.name + " " + it.links) }
-    val start = nodes.firstOrNull { it.name == "start" }!!
+    partOne()
+    paths = mutableListOf<String>()
+    partTwo()
+}
+
+fun partOne() {
     paths.add("")
-    Step(start, 0)
+    Step("start", 0)
     println("[part one] paths = ${paths.filter { it.endsWith("end") }.size}")
 }
 
-fun Step(curr: Node, pathIndex: Int) {
-    paths[pathIndex] += curr.name
-    if (curr.name == "end") {
+fun partTwo() {
+    paths.add("")
+    StepPartTwo("start", 0)
+    println("[part two] paths = ${paths.filter { it.endsWith("end") }.size}")
+}
+
+fun Step(curr: String, pathIndex: Int) {
+    paths[pathIndex] += curr
+    if (curr == "end") {
         return
     }
-    curr.links.forEach { link ->
-        val next = nodes.firstOrNull { it.name == link }!!
-        val isVisited = next.name == next.name.lowercase() && paths[pathIndex].contains(next.name)
+    val node = nodes.firstOrNull { it.name == curr }!!
+    node.links.forEach { next ->
+        val isVisited = next == next.lowercase() && paths[pathIndex].contains(next)
         if (!isVisited) {
             paths.add(paths[pathIndex] + " ") // MUST add a space to avoid false concatenation
             Step(next, paths.size - 1)
@@ -44,32 +54,30 @@ fun Step(curr: Node, pathIndex: Int) {
     }
 }
 
+fun StepPartTwo(curr: String, pathIndex: Int) {
+    paths[pathIndex] += curr
+    if (curr == "end") {
+        return
+    }
+    val doneSmall = paths[pathIndex].split(" ").filter { it != "start" && it == it.lowercase() }
+    val hasDoubleVisit = doneSmall.size > doneSmall.toSet().size
+    val node = nodes.firstOrNull { it.name == curr }!!
+    node.links.forEach { next ->
+        val isVisited = when {
+            next == "start" -> true
+            next == next.lowercase() && paths[pathIndex].contains(next) && hasDoubleVisit -> true
+            else -> false
+        }
+        if (!isVisited) {
+            paths.add(paths[pathIndex] + " ") // MUST add a space to avoid false concatenation
+            StepPartTwo(next, paths.size - 1)
+        }
+    }
+}
+
 data class Node(val name: String) {
     val links = mutableSetOf<String>()
 }
-
-/* 
-val input = listOf(
-    "fs", "end",
-    "he", "DX",
-    "fs", "he",
-    "start", "DX",
-    "pj", "DX",
-    "end", "zg",
-    "zg", "sl",
-    "zg", "pj",
-    "pj", "he",
-    "RW", "he",
-    "fs", "DX",
-    "pj", "RW",
-    "zg", "RW",
-    "start", "pj",
-    "he", "WI",
-    "zg", "he",
-    "pj", "fs",
-    "start", "RW"
-    )
-*/
 
 val input = listOf(
     "EO", "jc",
